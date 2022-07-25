763b3a7e4686ad5d85d4d037c239a70a0ece0a5f
Woochang Jeong <wcjeong@postech.ac.kr>
2022-07-25 20:58:16 +0900

    kinetic typography

---
+++ app.js
@@ -0,0 +1,69 @@
+import {Text} from './text.js';
+import {Visual} from './visual.js';
+
+class App {
+    constructor() {
+        this.setWebgl();
+
+        WebFont.load({
+            google: {
+                families: ['Hind:700']
+            },
+            fontactive: () => {
+
+                this.visual = new Visual();
+
+
+                window.addEventListener('resize', this.resize.bind(this), false);
+                this.resize();
+
+                requestAnimationFrame(this.animate.bind(this));
+                this.text = new Text();
+                this.text.setText(
+                    'NP',
+                    1,
+                    document.body.clientWidth,
+                    document.body.clientHeight,
+                );
+
+            }
+        });
+    }
+
+    setWebgl() {
+        this.renderer = new PIXI.Renderer({
+            width: document.body.clientWidth,
+            height: document.body.clientHeight,
+            antialias: true,
+            transparent: false,
+            resolution: (window.devicePixelRatio > 1) ? 2 : 1,
+            autoDensity: true,
+            powerPreference: "high-performance",
+            backgroundColor: 0xffffff,
+        });
+        document.body.appendChild(this.renderer.view);
+
+        this.stage = new PIXI.Container();
+    }
+
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        this.renderer.resize(this.stageWidth, this.stageHeight);
+
+        this.visual.show(this.stageWidth, this.stageHeight, this.stage);
+    }
+
+    animate(t) {
+        requestAnimationFrame(this.animate.bind(this));
+
+        this.visual.animate();
+
+        this.renderer.render(this.stage);
+    }
+}
+
+window.onload = () => {
+    new App();
+}
